63806cb2f939a4917c69b9684cc9a85b6f9303e7
Adamclark96 <adam.clark@huskers.unl.edu>
2021-11-23 14:01:07 -0600

    Final Commit

--- Tipsyr/app.R
+++ Tipsyr/app.R
@@ -6,7 +6,7 @@
 #
 #    http://shiny.rstudio.com/
 #
-
+library(plotly)
 library(shiny)
 library(tidyverse)
 library(stringr)
@@ -20,22 +20,34 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            textInput("text", label = h3("Enter ingredients here"), value = "Enter text..."),
+            textInput("text", label = h3("Enter ingredients here"), value = "beer"),
             hr(),
             fluidRow(column(3, verbatimTextOutput("value")))
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-          "Main Panel"
+          plotlyOutput("bar")
         )
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$value <- renderPrint({ input$text })
+    cocktaild = reactive({
+        phrase = input$text
+        return( cocktail%>% filter(agrepl(phrase, merged) == TRUE))
+    })
+        
+    
+    output$bar <- renderPlotly({
+        cocktail3 <- cocktaild()
+        
+      p <-  ggplot(data=cocktail3, aes(x=drink, y=ingredients, label = merged))+
+            geom_bar(stat="identity")+
+            theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
+        ggplotly(p)
+    })
     
 }
 
